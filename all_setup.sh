5464e21ec5723392cd70b414ad2ba8fe30782069
sabiqali <sabiq.work@gmail.com>
2022-04-27 13:14:31 -0400

    included setup scripts

---
+++ all_setup.sh
@@ -0,0 +1,10 @@
+#download and install the packages and dependencies needed by the SV analysis pipeline
+conda env create -n nanopore-SV-analysis -f ./nanopore-SV-analysis/nanopore-SV-analysis.yml
+
+#download and install the packages and dependencies needed by strdisc
+conda env create -n strdisc -f strdisc.yml
+conda activate strdisc
+
+#get references needed by SV analysis pipeline
+chmod u+x ./nanopore-SV-analysis/reference/download_reference.sh
+./nanopore-SV-analysis/reference/download_reference.sh
